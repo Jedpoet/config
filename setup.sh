9c8c15da212eefd98c5835ff01958010489917d8
jadepoet0231 <jadepoet0231@gmail.com>
2026-03-06 23:37:35 +0800

    feat: 腳本更新自動下載gh

--- setup.sh
+++ setup.sh
@@ -21,8 +21,23 @@ if ! command -v cargo &> /dev/null; then
     echo "正在安裝 Rust 工具鏈..."
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
     source "$HOME/.cargo/env"
+    rustup component add rust-analyzer
 else
     echo "Rust 已經安裝"
+fi
+
+if ! command --version gh &> /dev/null; then
+    (type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+else
+    echo "gh 已經安裝"
 fi
 
 # 安裝 Node.js 環境
